d138a46487eaf99e597c770eaf74ba7c0cfcc296
harith <hareth.cn12012@gmail.com>
2023-04-02 16:53:43 +0300

    Done

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 
-// index.js
-//     findMatching()
-//       ✓ returns all drivers that match the passed in name
-//       ✓ returns matching drivers if case does not match but letters do
-//       ✓ returns an empty array if there is no match
-    
-
 function findMatching (  drivers, name  ) 
 {
     return drivers.filter ( driver => driver.toLowerCase() === name.toLowerCase()  ) ;
 }
 
-
-// fuzzyMatch ( )
-//       1) returns a driver if beginning provided letters match
-//       2) does not return drivers if only middle or ending letters match
-//       3) does not return drivers if only middle or ending letters match
-    
 
 function fuzzyMatch(drivers, letters) {
     return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
